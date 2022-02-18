17086fc4dbc85754559eb239ba40b726d1a324b1
Halima Sadiq <67449331+halimasadiq@users.noreply.github.com>
2022-02-17 23:57:53 -0500

    removed comments

--- memperf.c
+++ memperf.c
@@ -7,48 +7,11 @@
 
 int main(){
 
-/*
-	myinit(1);
-	void*p1, *p2, *p3, *p4, *p5;
-	p1 = mymalloc(100);
-	printf("addr of malloc 1 %p\n",p1);
-
-	p2 = mymalloc(50);
-	printf("addr of malloc 2 %p\n",p2);
-	p3 = mymalloc(150);
-	printf("addr of malloc 3 %p\n",p3);
-	p4 = mymalloc(10);
-	printf("addr of malloc 4 %p\n",p4);
-	p5 = mymalloc(60);
-	printf("addr of malloc 5 %p\n",p5);
-	myfree(p2);
-//	myfree(p3);
-	printf("malloc of p2 after free\n\n\n");
-	printf("addr of %p\n", mymalloc(50));
-	myfree(p1);
-	printf("malloc after free %p\n",mymalloc(150));
-	printf("malloc 6 %p\n",mymalloc(200));
-	printf("malloc after free %p\n",mymalloc(150));
-	myfree(p2);
-	myfree(p4);
-	myfree(p5);
-	printf("malloc after 3 frees %p\n",mymalloc(70));
-	myfree(mymalloc(200));
-	printf("malloc after free %p\n",mymalloc(150));
-	printf("malloc after free %p\n",mymalloc(150));
-	printf("malloc after free %p\n",mymalloc(1000));
-	printf("malloc after free %p\n",mymalloc(15000));
-	printf("malloc after free %p\n",mymalloc(100050));
-	printf("malloc after free %p\n",mymalloc(5000));
-*/
-
-
 	int toMalloc  = 1;
 	int toRealloc = 1;
 	int todo;
 	double througput;
 	void* p;
-//	void* m;
 	time_t before;
 	time_t after;
 
@@ -60,23 +23,12 @@ int main(){
 		srand(time(0));
 		todo = rand() % 10 +1;
 		if(todo == 2){
-
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
 			myfree(p);
-//			i++;
 		}
 		if(todo == 3){
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
-
-
 			srand(time(0));
 			toRealloc = rand() % 200 + 100;
 			myrealloc(p,toMalloc+10);
-//			i++;
 		}
 		else{
 
@@ -99,28 +51,17 @@ int main(){
 
 	myinit(1);
 	time(&before);
-//	double f = 1;
 	p = mymalloc(100);
 	for(int i = 0; i<1000000; i++){
 		srand(time(0));
 		todo = rand() % 10 +1;
 		if(todo == 2){
-
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
 			myfree(p);
-//			i++;
 		}
 		if(todo == 3){
-
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
 			srand(time(0));
 			toRealloc = rand() % 200 + 100;
 			myrealloc(p,toRealloc);
-//			i++;
 		}
 		else{
 
@@ -142,36 +83,23 @@ int main(){
 
 	myinit(2);
 	time(&before);
-//	double f = 1;
 	p = mymalloc(100);
 	for(int i = 0; i<1000000; i++){
 		srand(time(0));
 		todo = rand() % 10 +1;
 		if(todo == 2){
-
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
 			myfree(p);
-//			i++;
 		}
 		if(todo == 3){
-
-//			srand(time(0));
-//			toMalloc = rand() % 256 +1;
-//			p = mymalloc(toMalloc);
 			srand(time(0));
 			toRealloc = rand() % 200 + 100;
 			myrealloc(p,toRealloc);
-//			i++;
 		}
 		else{
-
 			srand(time(0));
 			toMalloc = rand() % 256 +1;
 			p = mymalloc(toMalloc);
 		}
-		
 	}
 	time(&after);
 
